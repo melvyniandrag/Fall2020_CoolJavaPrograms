def1117a6fb59099ec0bd928db4e5c13516a422e
Roberto Ventura <rventura1@njcu.edu>
2020-10-11 17:37:55 -0400

    my java program

---
+++ RobertoVentura/mult.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+ 
+public class Exercise7 {
+ 
+ public static void main(String[] args) {
+  Scanner in = new Scanner(System.in);
+ 
+
+System.out.println("program that takes a number as input and prints its multiplication table upto 10");
+
+  System.out.print("Input a number: ");
+  int num1 = in.nextInt();
+   
+  for (int i=0; i< 10; i++){
+   System.out.println(num1 + " x " + (i+1) + " = " + 
+     (num1 * (i+1)));
+  }
+ }
+}
+
