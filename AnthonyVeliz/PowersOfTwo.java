7baa4502a914e7d5af99b31b548dcd21704c4f1f
Anthony Veliz <aveliz93@gmail.com>
2020-10-14 20:12:15 +0000

    Java file added

---
+++ AnthonyVeliz/PowersOfTwo.java
@@ -0,0 +1,13 @@
+public class PowersOfTwo
+{
+   public static void main(String[] args)
+   {  
+      
+      int x= 2;
+      for(int y = 0; y<= 20; y ++)
+      {
+         double answer = Math.pow(x,y);
+         System.out.println(answer);
+      }
+   }
+}
