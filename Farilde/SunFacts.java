05367ef1dfdff16fadde1b247d8735d1e9769723
Farilde Porquin <fporquin@njcu.edu>
2020-10-15 02:41:49 +0000

    7th time

---
+++ Farilde/SunFacts.java
@@ -0,0 +1,25 @@
+public class SunFacts
+{ 
+        public static void main(String[] args)
+        {
+                 double distance, mass;
+                 double  mass = 1.989E30;
+System.out.println("The sun is " + distance + " meters away.");
+System.out.println("The sun's mass is " + mass + " kilograms.");
+        }
+}
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+-- INSERT --                                                  7,58          All
+
